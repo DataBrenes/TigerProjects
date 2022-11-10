25df699f60a5cf470b4748d7895009a3806edde1
DataBrenes <mike@brenesfinancial.com>
2022-11-10 00:39:59 -0500

    Updated scraper to select 100 rows of table and select new tab

--- Scrapers/GrandWelcome/Scraper/app/newbookings.py
+++ Scrapers/GrandWelcome/Scraper/app/newbookings.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import Select
 import pandas as pd
 from time import sleep
 from datetime import datetime
@@ -15,6 +16,7 @@ import chk_new_res as cn
 import build_payload as bp
 import send2cal as cal
 import logging
+import re
 
 logging.basicConfig(filename='logs/booking.log', level=logging.INFO)
 
@@ -23,6 +25,27 @@ def check_new(df):
         logging.info('No new booking found.')
     else:
         logging.info('New booking found.')
+
+def CountDaysMonth(Arrival,Departure):    
+    from datetime import date, timedelta
+    from dateutil.parser import parse
+    from datetime import datetime
+    BookedDaysPerMonth = {}
+    d1 = parse(Arrival).date()
+    d2 = parse(Departure).date()
+    # this will give you a list containing all of the dates
+    times = [d1 + timedelta(days=x) for x in range((d2-d1).days + 1)]
+    # Creates a dictionary and increments if it exists 
+    for t in times:
+        month = t.strftime("%b")+'-'+t.strftime("%Y")
+        if month in BookedDaysPerMonth:
+            BookedDaysPerMonth[month] += 1
+        else:
+            BookedDaysPerMonth[month] = 1
+    Dates_list=[]
+    for k,v in BookedDaysPerMonth.items():
+        Dates_list.append('{}-{}'.format(k,v))
+    return Dates_list
 
 
 sleep(15)
@@ -50,15 +73,42 @@ while True:
     restab=browser.find_element(by=By.XPATH, value="//span[normalize-space()='Reservations']")
     restab.click()
     sleep(5)
+
+    ## Need to make sure that 100 is showing instead of 10
+    select = Select(browser.find_element(by=By.XPATH, value="//*[@id='table_length']/label/select"))
+    # select by value 
+    select.select_by_value('100')
+
+    # check if another page 
+    table=browser.find_element(by=By.XPATH, value='//*[@id="table_paginate"]/ul')
+    items = table.text.split()
+    items.remove('Previous')
+    items.remove('Next')
+
     ## download reservations 
     # export=browser.find_element(by=By.XPATH, value="//input[@value='Export']")
     # export.click()
     # sleep(2)
+# multiple pages
+    webtables = []
+    for page in items:
+        # set to 100 
+        select = Select(browser.find_element(by=By.XPATH, value="//*[@id='table_length']/label/select"))
+        # select by value 
+        select.select_by_value('100')
+        # get reservations from web
+        html = browser.page_source
+        table = pd.read_html(html)
+        res_raw_df = table[0]
+        webtables.append(res_raw_df)
+        # click next page 
+        nxt=browser.find_element(by=By.XPATH, value='//*[@id="table_next"]/a')
+        nxt.click()  
     
-    # get reservations from web
-    html = browser.page_source
-    table = pd.read_html(html)
-    res_df = table[0]
+    combined=pd.concat(webtables)
+    res_df=combined[['Res. #', 'Status', 'Unit', 'Guest', 'Booked Date', 'Check-In','Checkout', 'Nights', 'Income']]
+
+
     res_file=gp.get_params('reservations')
     # check if res id exists
     curr = pd.read_csv(res_file['all_res'])
@@ -82,6 +132,46 @@ while True:
     master_df.reset_index(drop=True,inplace=True)
     master_df.to_csv(res_file['all_res'],index=False)
     logging.info("Updating Master List")
+
+    
+    ## merge with old data 
+    logging.info("Creating Summary Table")
+    # format dates to match for merging
+    res_df=res_df.copy()
+    res_df["Booked Date"] = pd.to_datetime(res_df["Booked Date"]).dt.strftime('%b/%d/%Y')
+    res_df["Check-In"] = pd.to_datetime(res_df["Check-In"]).dt.strftime('%b/%d/%Y')
+    res_df["Checkout"] = pd.to_datetime(res_df["Checkout"]).dt.strftime('%b/%d/%Y')
+    
+    rpts=gp.get_params('reports')
+    old=pd.read_csv(rpts['old'])
+    frames = [res_df,old]
+    summ=pd.concat(frames)
+
+    # create a summary table
+    summ1=summ[['Check-In','Checkout', 'Nights', 'Income']]
+    nightly_rate = []
+    for i,b in summ1.iterrows():
+        Arrival = b['Check-In']
+        Departure = b['Checkout']
+        Income_raw = b['Income']
+        Income= float(re.sub(r'[^\d.]', '', Income_raw))
+        times=CountDaysMonth(Arrival,Departure)
+
+        # times_df = pd.DataFrame(times) 
+        # times_df.to_csv(rpts['test'],index=False)
+        # summ1.to_csv(rpts['test2'],index=False)
+
+        for t in times:
+            nightly = Income / len(times)
+            night_form = '{0:.2f}'.format(nightly)
+            t_stmp=pd.Timestamp(t)
+            nightly_rate.append([t_stmp,float(night_form)])
+    breakdown = pd.DataFrame(nightly_rate, columns = ['Date', 'Per Night'])
+    breakdown['Month-Year']=breakdown['Date'].dt.strftime('%m/%Y')
+    rpts=gp.get_params('reports')
+    breakdown.to_csv(rpts['breakdown'],index=False)
+
+
 
     logging.info("Closing Browser")
     browser.close()
