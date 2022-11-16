4c1db0604f2ba7f1ce0dd6b3b19ebe537a967154
databrenes <mikebrenes@tigerteamanalytics.com>
2022-11-15 23:41:59 -0500

    fixed the date formats

--- Scrapers/GrandWelcome/Scraper/app/build_payload.py
+++ Scrapers/GrandWelcome/Scraper/app/build_payload.py
@@ -3,8 +3,8 @@ from datetime import datetime
 def build_pl(rec):
     inTime='04:00PM'
     outTime='10:00AM'
-    ch_in=datetime.strptime(rec['Check-In']+' '+inTime, '%b %d, %Y %I:%M%p').strftime("%Y-%m-%dT%H:%M:%S")
-    ch_out=datetime.strptime(rec['Checkout']+' '+outTime, '%b %d, %Y %I:%M%p').strftime("%Y-%m-%dT%H:%M:%S")
+    ch_in=datetime.strptime(rec['Check-In']+' '+inTime, '%b/%d/%Y %I:%M%p').strftime("%Y-%m-%dT%H:%M:%S")
+    ch_out=datetime.strptime(rec['Checkout']+' '+outTime, '%b/%d/%Y %I:%M%p').strftime("%Y-%m-%dT%H:%M:%S")
     pl_json={
         "calStart":ch_in,
         "calEnd":ch_out,
